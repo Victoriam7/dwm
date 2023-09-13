ef71408c51669d1e7d37d8b748435776105f88d7
Brendan Grau <bsg8376@g.rit.edu>
2023-09-13 15:49:23 -0400

    switch to using keycodes for config

--- config.def.h
+++ config.def.h
@@ -89,7 +89,6 @@ ResourcePref resources[] = {
 
 
 static const Key keys[] = {
-#if 0
 	/* modifier                        key              function                argument */
 	/* { MODKEY,                       XK_a,            noop,                   {} }, */
 	/* { MODKEY|ControlMask,           XK_a,            noop,                   {} }, */
@@ -200,7 +199,7 @@ static const Key keys[] = {
 	/* { MODKEY|ControlMask,           XK_Tab,          noop,                   {} }, */
 	/* { MODKEY|ControlMask|ShiftMask, XK_Tab,          noop,                   {} }, */
 	   { MODKEY,                       XK_Return,       spawn,                  {.v=termcmd} }, 
-	   { MODKEY|ShiftMask,             XK_Return,       togglescratch,          {.ui=0} }, 
+	/* { MODKEY|ShiftMask,             XK_Return,       togglescratch,          {.ui=0} }, */
 	/* { MODKEY|ControlMask,           XK_Return,       noop,                   {} }, */
 	/* { MODKEY|ControlMask|ShiftMask, XK_Return,       noop,                   {} }, */
 	/* { MODKEY,                       XK_Delete,       noop,                   {} }, */
@@ -257,7 +256,7 @@ static const Key keys[] = {
 	/* { MODKEY|ControlMask,           XK_underscore,   noop,                   {} }, */
 	/* { MODKEY,                       XK_apostrophe,   noop,                   {} }, */
 	/* { MODKEY|ControlMask,           XK_apostrophe,   noop,                   {} }, */
-	   { MODKEY,                       XK_quotedbl,     togglescratch,          {.ui=1} },
+	/* { MODKEY,                       XK_quotedbl,     togglescratch,          {.ui=1} }, */
 	/* { MODKEY|ControlMask,           XK_quotedbl,     noop,                   {} }, */
 	/* { MODKEY,                       XK_dollar,       noop,                   {} }, */
 	/* { MODKEY|ControlMask,           XK_dollar,       noop,                   {} }, */
@@ -350,42 +349,6 @@ static const Key keys[] = {
              TAGKEYS(                      XK_7,                                    6)
              TAGKEYS(                      XK_8,                                    7)
              TAGKEYS(                      XK_9,                                    8)
-#endif 
-        /* modifier                     key        function        argument */
-        { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-        { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-        { MODKEY,                       XK_b,      togglebar,      {0} },
-        { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-        { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-        { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-        { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-        { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-        { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-        { MODKEY,                       XK_Return, zoom,           {0} },
-        { MODKEY,                       XK_Tab,    view,           {0} },
-        { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-        { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-        { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-        { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-        { MODKEY,                       XK_space,  setlayout,      {0} },
-        { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-        { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-        { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-        { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-        { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-        { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-        { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-        TAGKEYS(                        XK_1,                      0)
-        TAGKEYS(                        XK_2,                      1)
-        TAGKEYS(                        XK_3,                      2)
-        TAGKEYS(                        XK_4,                      3)
-        TAGKEYS(                        XK_5,                      4)
-        TAGKEYS(                        XK_6,                      5)
-        TAGKEYS(                        XK_7,                      6)
-        TAGKEYS(                        XK_8,                      7)
-        TAGKEYS(                        XK_9,                      8)
-        { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
 };
 
 /* button definitions */
